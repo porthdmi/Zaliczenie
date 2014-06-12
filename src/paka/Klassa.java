f3221a2b77fa5e4ff8e2582a1a36ac364bb66212
Tomasz_Brynza <port.hdmi@gmail.com>
2014-06-12 15:30:00 +0200

    Dodanie bibliotek java

--- src/paka/Klassa.java
+++ src/paka/Klassa.java
@@ -4,6 +4,14 @@ package paka;
  *
  * @autor Tomasz Brynza & Emilka Rogalska
  */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.Border;
+import java.util.*;
+
+
 public class klasa {
 
     public static void main(String[] args) {
