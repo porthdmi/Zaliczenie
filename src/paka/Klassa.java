5ce1f1f60dda9d70889e6f03a274635dcd004636
Tomasz_Brynza <port.hdmi@gmail.com>
2014-06-12 15:34:16 +0200

    Wstawienie nowej klasy Klassa

--- src/paka/Klassa.java
+++ src/paka/Klassa.java
@@ -12,10 +12,77 @@ import javax.swing.border.Border;
 import java.util.*;
 
 
-public class klasa {
+public class Klassa {
+    GallowsArea gallowsArea = null;
 
-    public static void main(String[] args) {
-        
-    }
+    JButton exitButton    = null;
+    JButton newGameButton = null;
+
+    JLabel    wordArea    = null;
+    JLabel    messageArea = null;
+    java.util.List alphaButtonList = new ArrayList();
+    Iterator alphaIterator = null;
+
+    boolean reset        = true;
+    boolean disable      = false;
+    boolean dontWrap     = false;
+    boolean wrap         = true;
+    boolean headDrawn    = false;
+    boolean bodyDrawn    = false;
+    boolean leftArmDrawn = false;
+    boolean rightArmDrawn= false;
+    boolean leftLegDrawn = false;
+    boolean rightLegDrawn= false;
+
     
+    String[] targetWords = {"program","informatyka","komputer" };
+    String winnerMessage = "Gratulacje!  Jesteœ ocalony!";
+    String losingPrefix  = "Ÿle! Umierasz. Chodzi³o o inne s³owo.. ";
+    String currentGuess;
+    String targetWord;
+
+    int numberWrong       = 0;
+    int numberOfBodyParts = 6;
+    int next              = 0;
+
+    public void setUpNewGame() {
+        numberWrong = 0;
+        messageArea.setText("wygraj albo zgiñ!");
+
+        
+        Iterator alphaIterator = alphaButtonList.iterator();
+        while( alphaIterator.hasNext() ) {
+            ( (JButton)alphaIterator.next() ).setEnabled( reset );
+        }
+
+        
+        newGameButton.setEnabled( disable );
+
+      
+        wordArea.setBackground(Color.lightGray);
+
+        
+        double numb = Math.random();
+        next = (int)( numb * targetWords.length );
+        targetWord  = targetWords[next];
+
+       
+        currentGuess = "?";
+        for( int i=0; i<targetWord.length()-1; i++) {
+            currentGuess = currentGuess.concat("?");
+        }
+        wordArea.setText( currentGuess );
+
+        
+        headDrawn    = false;
+        bodyDrawn    = false;
+        leftArmDrawn = false;
+        rightArmDrawn= false;
+        leftLegDrawn = false;
+        rightLegDrawn= false;
+        gallowsArea.repaint();
+
+    }
+
+
 }
