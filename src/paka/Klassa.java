9ddf98ae2dee1ce349a914cdc09de0241f7000ac
Tomasz_Brynza <port.hdmi@gmail.com>
2014-06-12 15:39:47 +0200

    dodanie panelu ,przyciskow button(a-m)

--- src/paka/Klassa.java
+++ src/paka/Klassa.java
@@ -102,5 +102,218 @@ public class Klassa {
             }
         }
 
+currentGuess = nextGuess;
+        wordArea.setText( currentGuess );
+
+       
+        if( currentGuess.equals( targetWord ) ) {
+            
+            Iterator alphaIterator = alphaButtonList.iterator();
+            while( alphaIterator.hasNext() ) {
+                ( (JButton)alphaIterator.next() ).setEnabled( disable );
+            }
+            messageArea.setText( winnerMessage );
+            newGameButton.setEnabled( reset );
+            exitButton.setEnabled( reset );
+        }
+        
+        else {
+            if( !foundAMatch ) {
+                numberWrong++;
+                switch (numberWrong){
+                    case 1: { headDrawn     = true; break; }
+                    case 2: { bodyDrawn     = true; break; }
+                    case 3: { leftArmDrawn  = true; break; }
+                    case 4: { rightArmDrawn = true; break; }
+                    case 5: { leftLegDrawn  = true; break; }
+                    case 6: { rightLegDrawn = true; break; }
+                    default: System.out.println("powinienieœ zgin¹æ!");
+                }
+                
+                gallowsArea.repaint();
+            }
+            
+            if( numberWrong >= numberOfBodyParts ) {
+                
+                Iterator alphaIterator = alphaButtonList.iterator();
+                while( alphaIterator.hasNext() ) {
+                    ( (JButton)alphaIterator.next() ).setEnabled( disable );
+                }
+                messageArea.setText( losingPrefix + targetWord );
+                newGameButton.setEnabled( reset );
+                exitButton.setEnabled( reset );
+            }
+        }
+    }
+
+
+    public Component createNorthPane() {
+        JPanel pane = new JPanel();
+        pane.setLayout( new BoxLayout( pane, BoxLayout.X_AXIS ) );
+        pane.setBorder( BorderFactory.createEmptyBorder(0, 10, 10, 10) );
+        pane.add(Box.createHorizontalGlue() );
+        wordArea = new JLabel("Nowa gra");
+        wordArea.setFont( new Font("Helvetica", Font.PLAIN, 24) );
+        wordArea.setBackground(Color.lightGray);
+        wordArea.setForeground(Color.black);
+        pane.add(wordArea);
+        pane.add(Box.createHorizontalGlue() );
+        return pane;
+    }
+
+
+
+
+    public Component createWestPane() {
+        ActionListener alphabetButtonAction = new ActionListener() {
+            public void actionPerformed( ActionEvent e ) {
+                JButton buttonPushed = (JButton)e.getSource();
+                buttonPushed.setEnabled( disable );
+                processAnswer( buttonPushed.getText() );
+            }
+        };
+
+        JPanel pane = new JPanel();
+        pane.setBorder(BorderFactory.createLoweredBevelBorder());
+        GridBagLayout gridbag = new GridBagLayout();
+        GridBagConstraints c  = new GridBagConstraints();
+        pane.setLayout( gridbag );
+        c.fill = GridBagConstraints.BOTH;
+
+        JButton button = new JButton( "a" );
+        c.weightx    = 0.5;
+        c.weighty    = 0.5;
+        c.gridx      = 0;
+        c.gridy      = 0;
+        c.gridheight = 1;
+        gridbag.setConstraints( button, c );
+        button.addActionListener( alphabetButtonAction );
+        pane.add( button );
+        alphaButtonList.add( button );
+
+        button = new JButton( "b" );
+        c.weightx    = 0.5;
+        c.weighty    = 0.5;
+        c.gridx      = 1;
+        c.gridy      = 0;
+        gridbag.setConstraints( button, c );
+        button.addActionListener( alphabetButtonAction );
+        pane.add( button );
+        alphaButtonList.add( button );
+
+        button = new JButton( "c" );
+        c.weightx    = 0.5;
+        c.weighty    = 0.5;
+        c.gridx      = 2;
+        c.gridy      = 0;
+        gridbag.setConstraints( button, c );
+        button.addActionListener( alphabetButtonAction );
+        pane.add( button );
+        alphaButtonList.add( button );
+
+        button = new JButton( "d" );
+        c.weightx    = 0.5;
+        c.weighty    = 0.5;
+        c.gridx      = 0;
+        c.gridy      = 1;
+        gridbag.setConstraints( button, c );
+        button.addActionListener( alphabetButtonAction );
+        pane.add( button );
+        alphaButtonList.add( button );
+
+        button = new JButton( "e" );
+        c.weightx    = 0.5;
+        c.weighty    = 0.5;
+        c.gridx      = 1;
+        c.gridy      = 1;
+        gridbag.setConstraints( button, c );
+        button.addActionListener( alphabetButtonAction );
+        pane.add( button );
+        alphaButtonList.add( button );
+
+        button = new JButton( "f" );
+        c.weightx    = 0.5;
+        c.weighty    = 0.5;
+        c.gridx      = 2;
+        c.gridy      = 1;
+        gridbag.setConstraints( button, c );
+        button.addActionListener( alphabetButtonAction );
+        pane.add( button );
+        alphaButtonList.add( button );
+
+
+        button = new JButton( "g" );
+        c.weightx    = 0.5;
+        c.weighty    = 0.5;
+        c.gridx      = 0;
+        c.gridy      = 2;
+        gridbag.setConstraints( button, c );
+        button.addActionListener( alphabetButtonAction );
+        pane.add( button );
+        alphaButtonList.add( button );
+
+        button = new JButton( "h" );
+        c.weightx    = 0.5;
+        c.weighty    = 0.5;
+        c.gridx      = 1;
+        c.gridy      = 2;
+        gridbag.setConstraints( button, c );
+        button.addActionListener( alphabetButtonAction );
+        pane.add( button );
+        alphaButtonList.add( button );
+
+        button = new JButton( "i" );
+        c.weightx    = 0.5;
+        c.weighty    = 0.5;
+        c.gridx      = 2;
+        c.gridy      = 2;
+        gridbag.setConstraints( button, c );
+        button.addActionListener( alphabetButtonAction );
+        pane.add( button );
+        alphaButtonList.add( button );
+
+        button = new JButton( "j" );
+        c.weightx    = 0.5;
+        c.weighty    = 0.5;
+        c.gridx      = 0;
+        c.gridy      = 3;
+        gridbag.setConstraints( button, c );
+        button.addActionListener( alphabetButtonAction );
+        pane.add( button );
+        alphaButtonList.add( button );
+
+        button = new JButton( "k" );
+        c.weightx    = 0.5;
+        c.weighty    = 0.5;
+        c.gridx      = 1;
+        c.gridy      = 3;
+        gridbag.setConstraints( button, c );
+        button.addActionListener( alphabetButtonAction );
+        pane.add( button );
+        alphaButtonList.add( button );
+
+        button = new JButton( "l" );
+        c.weightx    = 0.5;
+        c.weighty    = 0.5;
+        c.gridx      = 2;
+        c.gridy      = 3;
+        gridbag.setConstraints( button, c );
+        button.addActionListener( alphabetButtonAction );
+        pane.add( button );
+        alphaButtonList.add( button );
+
+
+
+	button = new JButton( "m" );
+        c.weightx    = 0.5;
+        c.weighty    = 0.5;
+        c.gridx      = 0;
+        c.gridy      = 4;
+        gridbag.setConstraints( button, c );
+        button.addActionListener( alphabetButtonAction );
+        pane.add( button );
+        alphaButtonList.add( button );
+
+
 
 }
